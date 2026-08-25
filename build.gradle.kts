f95bd6ce646c871d820883998d2b2f5da91d4140
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-08-25 12:39:36 +0200

    refactor: bytt til io.github.ben-manes.versions plugin-id

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
     id("org.springframework.boot") version "3.5.16"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
-    id("com.github.ben-manes.versions") version "0.61.0"
+    id("io.github.ben-manes.versions") version "0.61.0"
 }
 
 group = "no.novari"
