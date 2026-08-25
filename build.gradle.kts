82104ba4a5895d37b47c199bab7726c576eea41e
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-08-25 08:43:44 +0200

    fix: overstyr sårbare avhengigheter på buildscript-classpath
    Dependabot rapporterer httpclient5, httpcore5, httpcore5-h2,
    jackson-databind og commons-lang3 som sårbare. Versjonene kommer fra
    Gradle sin plugin-classpath via spring-boot-gradle-plugin, ikke fra
    applikasjonen. extra[...]-overstyringene treffer kun prosjektets egne
    konfigurasjoner og har derfor ingen effekt her.
    
    Constraints på classpath-konfigurasjonen løfter dem i stedet.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,20 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
+buildscript {
+    repositories {
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath(platform("com.fasterxml.jackson:jackson-bom:2.22.2"))
+        constraints {
+            classpath("org.apache.httpcomponents.client5:httpclient5:5.6.4")
+            classpath("org.apache.httpcomponents.core5:httpcore5:5.4.3")
+            classpath("org.apache.httpcomponents.core5:httpcore5-h2:5.4.3")
+            classpath("org.apache.commons:commons-lang3:3.18.0")
+        }
+    }
+}
+
 plugins {
     kotlin("jvm") version "2.4.10"
     kotlin("plugin.spring") version "2.4.10"
